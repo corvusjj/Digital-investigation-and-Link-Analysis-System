26188131964924ab5a959042e563c733c90761d7
John Corbet <mjohncorbet@gmail.com>
2026-08-23 11:43:57 +0800

    create sample entity & relationship class

---
+++ main.py
@@ -0,0 +1,16 @@
+class Entity:
+    id = "POO1"
+
+    def __init__(self, type, name):
+        self.type = type
+        self.name = name
+
+class Relation:
+    def __init__(self, type, source, target):
+        self.type = type
+        self.source = source
+        self.target = target
+
+person1 = Entity('PERSON', 'Alex Cruz')
+relation1 = Relation('OWNS', 'P001', 'E001')
+print(person1.name)
