0df39a5e1018771633a312704115cf1b69c7dcc0
John Corbet <mjohncorbet@gmail.com>
2026-08-25 09:17:31 +0800

    initialize node ids module

---
+++ node_ids.py
@@ -0,0 +1,9 @@
+class IDs:
+     def __init__(self, nodeType, type):
+          self.relationIds = []
+          self.entityIds = []
+
+     def createID(self):
+                self.entityIds.append('P001')
+
+# type get first 2 chars for ID
