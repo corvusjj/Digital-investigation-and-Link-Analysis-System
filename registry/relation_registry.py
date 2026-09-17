aee90ec6dfe9bf8c021e61abf68ed1869df1eb0b
John Corbet <mjohncorbet@gmail.com>
2026-09-17 21:27:08 +0800

    add relation types registry

---
+++ registry/relation_registry.py
@@ -0,0 +1,12 @@
+RELATION_TYPES = [
+    "OWNS",
+    "USES",
+    "KNOWS",
+    "WORKS_FOR",
+    "LOCATED_AT",
+    "CONNECTED_TO",
+    "CONTACTED",
+    "MEMBER_OF",
+    "REGISTERED_TO",
+    "SEEN_AT"
+]
